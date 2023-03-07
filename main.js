35b2ccc20e33032f2ebaf34f7ca8785d999fa507
ScuffedGuy <110801525+ScuffedItalian@users.noreply.github.com>
2023-03-06 21:06:34 -0800

    Added NSFW LOL

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const img = document.getElementById("img");
-const button1 = document.getElementById("button1")
-const button2 = document.getElementById("button2")
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+
 async function getApiNeko() {
   const data1 = fetch("https://nekos.life/api/v2/img/neko");
   const response = await data1;
@@ -15,6 +18,15 @@ async function getApiWaifu() {
   img.src = data4.url;
 }
 
-//
+async function getApiWaifuNSFW() {
+  const data5 = fetch("https://api.waifu.pics/nsfw/waifu");
+  const response3 = await data5;
+  const data6 = await response3.json();
+  img.src = data6.url;
+}
 button1.addEventListener("click", getApiNeko)
 button2.addEventListener("click", getApiWaifu)
+button3.addEventListener("click", function() {
+  button4.style.display = "block"
+})
+button4.addEventListener("click", getApiWaifuNSFW)
