be2f3348fada1789d4aa9bf53d17728ac4a5fa67
ScuffedGuy <110801525+ScuffedItalian@users.noreply.github.com>
2023-03-06 18:22:06 -0800

    Waifu gen added?!?! OMG?!?!?! FR!?!?!?

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const img = document.getElementById("img");
-const button = document.getElementById("button1")
-async function getApi() {
+const button1 = document.getElementById("button1")
+const button2 = document.getElementById("button2")
+async function getApiNeko() {
   const data1 = fetch("https://nekos.life/api/v2/img/neko");
   const response = await data1;
   const data2 = await response.json();
   img.src = data2.url;
 }
 
+async function getApiWaifu() {
+  const data3 = fetch("https://api.waifu.pics/sfw/slap");
+  const response2 = await data3;
+  const data4 = await response2.json();
+  img.src = data4.url;
+}
 
-button.addEventListener("click", getApi)
+//
+button1.addEventListener("click", getApiNeko)
+button2.addEventListener("click", getApiWaifu)
